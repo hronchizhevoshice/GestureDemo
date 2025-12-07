5b9b009b0b6e151a229335ad78f616ce7526c367
hronchizhevoshice <hronchizhevoshice@gmail.com>
2025-12-07 14:31:33 +0700

    Add composable fun MainScreen

--- app/src/main/java/com/example/gesturedemo/MainActivity.kt
+++ app/src/main/java/com/example/gesturedemo/MainActivity.kt
@@ -20,8 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             GestureDemoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    MainScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +30,15 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen(modifier: Modifier = Modifier) {
+
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     GestureDemoTheme {
-        Greeting("Android")
+       MainScreen()
     }
 }
